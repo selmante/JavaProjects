9510dd48be2961ef84518c3a27c26e8dafc18dae
selman <selmantetik@hotmail.com>
2020-01-26 16:22:03 +0000

    committing whole project

--- src/com/sprintqa/java/reviewclass/ArrayExampleHomework.java
+++ src/com/sprintqa/java/reviewclass/ArrayExampleHomework.java
@@ -10,21 +10,22 @@ int temp=0;
 int indexOfMin=0;
 for( int i=0; i<numbers.length-1; i++)
 {
-min=numbers[0];
+min=numbers[i];
 for (int j=i; j<numbers.length; j++)
 {if (numbers[j]<min) {
 	min=numbers[j];
 	indexOfMin= j;
 }
-	}
+}
 //swapping of two numbers - numbers[indexOfMin], numbers[i]
 temp = numbers[indexOfMin];
 numbers[indexOfMin]=numbers[i];
 numbers[i]= temp;
-}
 
+}
 for (int i =0; i<numbers.length; i++)
 System.out.println(numbers[i]);
-
 }
+ 
+
 }
